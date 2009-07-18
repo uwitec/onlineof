86bb0cbe2359580f6f48c471ecf8fc04e0ebb2db
tandong386@163.com <tandong386@163.com@4fa35fc8-6e37-11de-b181-cfa5ed37d7b3>
2009-07-18 15:52:18 +0000

    update

---
+++ trunk/cd-onlineof/src/com/cd_help/onlineOF/web/dwr/LoadPrivilegeTreeAction.java
@@ -0,0 +1,33 @@
+/*
+ * 版权声明：
+ * 此文档的版权归常德help信息科技有限公司所有。
+ * 未征得常德help信息科技有限公司的书面批准，不得向第三方借阅、出让、出版该文档。
+ */
+package com.cd_help.onlineOF.web.dwr;
+
+import java.util.List;
+
+import com.cd_help.onlineOF.data.RestaurantData;
+
+/**
+ * <b><code></code></b>
+ * <p/>
+ * DWR加载餐厅订餐信息类
+ * <p/>
+ * <b>Creation Time:</b> Jul 4, 2009
+ * @author TanDong
+ * @version 0.0.0.1
+ *
+ * @since cd_help-onlineOF 0.0.0.1
+ */
+public class LoadPrivilegeTreeAction {
+
+	/**
+	 * 加载订餐排行信息
+	 * @return
+	 * @since cd_help-onlineOF 0.0.0.1
+	 */
+	public List<RestaurantData> loadInfo(){
+		return null;
+	}
+}
