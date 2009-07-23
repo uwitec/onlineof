9ff23098a697eee172e19cef688cc6bcc16b08c1
tandong386@163.com <tandong386@163.com@4fa35fc8-6e37-11de-b181-cfa5ed37d7b3>
2009-07-23 16:19:23 +0000

    update

--- trunk/cd-onlineof/src/com/cd_help/onlineOF/web/struts/UsersAction.java
+++ trunk/cd-onlineof/src/com/cd_help/onlineOF/web/struts/UsersAction.java
@@ -5,6 +5,7 @@
  */
 package com.cd_help.onlineOF.web.struts;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Service;
 
 import com.cd_help.onlineOF.utils.AppException;
 import com.cd_help.onlineOF.utils.PageBean;
+import com.cd_help.onlineOF.web.vo.RestaurantVo;
 import com.cd_help.onlineOF.web.vo.UsersVo;
 
 @SuppressWarnings("serial")
@@ -24,7 +26,10 @@ public class UsersAction extends BaseAction {
 	private UsersVo usersVo;
 	private List<UsersVo> usersVoList;
 	private PageBean pb;
-	private int page = 1;
+	private int currentPage = 1;
+	private String usersname;
+	private int restaurantId;
+	private List<RestaurantVo> restaurantVos = new ArrayList<RestaurantVo>();
 
 	/**
 	 * 加载所有用户
@@ -32,21 +37,34 @@ public class UsersAction extends BaseAction {
 	 * @return
 	 * @since cd_help-onlineOF 0.0.0.1
 	 */
-	public String loadAllUsers() {
+	public String searchUsersByPage() {
 		log.debug("--->> begin loadAllUsers");
+		String params[] = null;
+		Object conditions[] = null;
+		String hql = "searchUsersByPage";
 		try {
-			// usersVoList =
-			// this.getOnlineOF().getUsersManager().loadAll(this.getSession());
-			this.pb.setCurrentPage(page);
+			this.pb.setCurrentPage(currentPage);
 			this.pb.setPagesize(2);
-			this.pb = this.getOnlineOF().getUsersManager().loadAll("loadAllByPage",
-					null, null, this.pb, this.getSession());
+			if(null != usersname && usersname.length() > 0){
+				log.debug("--->> search by usersname"+"  usersname: "+usersname);
+				params = new String[]{"usersname"};
+				conditions = new Object[]{"%"+this.usersname+"%"};
+				hql = "searchByUsersname";
+			}
+			// 只有超级用户采用   根据餐厅去查用户
+			if(this.getSession().getUsersVo().getIsSuper() == 1){
+			   log.debug("--->> 超级用户");	
+			}
+			// 加载所有餐厅
+			restaurantVos = this.getOnlineOF().getRestaurantManager().loadAll();
+			this.pb = this.getOnlineOF().getUsersManager().searchByPage(hql,
+					params, conditions, this.pb, this.getSession());
 		} catch (AppException e) {
 			e.printStackTrace();
 		}
 		return SUCCESS;
 	}
-
+	
 	/**
 	 * 删除用户
 	 * 
@@ -58,7 +76,7 @@ public class UsersAction extends BaseAction {
 		try {
 			this.getOnlineOF().getUsersManager().delete(this.getSession(),
 					usersId);
-			this.loadAllUsers();
+			this.searchUsersByPage();
 		} catch (AppException e) {
 			e.printStackTrace();
 		}
@@ -110,17 +128,41 @@ public class UsersAction extends BaseAction {
 		return pb;
 	}
 
+	public String getUsersname() {
+		return usersname;
+	}
+
+	public void setUsersname(String usersname) {
+		this.usersname = usersname;
+	}
+
+	public int getRestaurantId() {
+		return restaurantId;
+	}
+
+	public void setRestaurantId(int restaurantId) {
+		this.restaurantId = restaurantId;
+	}
+
 	@Autowired
 	@Resource(name = "pageBean")
 	public void setPb(PageBean pb) {
 		this.pb = pb;
 	}
 
-	public int getPage() {
-		return page;
+	public int getCurrentPage() {
+		return currentPage;
 	}
 
-	public void setPage(int page) {
-		this.page = page;
+	public void setCurrentPage(int currentPage) {
+		this.currentPage = currentPage;
+	}
+
+	public List<RestaurantVo> getRestaurantVos() {
+		return restaurantVos;
+	}
+
+	public void setRestaurantVos(List<RestaurantVo> restaurantVos) {
+		this.restaurantVos = restaurantVos;
 	}
 }
