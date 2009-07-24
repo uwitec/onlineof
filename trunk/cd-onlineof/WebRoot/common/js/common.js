907779feecaaa77bb0c6e7461a3bfc99ee83f7eb
tandong386@163.com <tandong386@163.com@4fa35fc8-6e37-11de-b181-cfa5ed37d7b3>
2009-07-24 17:20:53 +0000

    add getAllCheckedValue

--- trunk/cd-onlineof/WebRoot/common/js/common.js
+++ trunk/cd-onlineof/WebRoot/common/js/common.js
@@ -1,1 +1,18 @@
 /***************公共的JS方法*********************/
+function getAllCheckedValue(){
+	var selectCount = 0;
+	var values = new Array();
+	var checks = document.getElementsByName("checks");
+	for(var i=0; i<checks.length; i++){
+		if(checks[i].checked){
+			values[i] = checks[i].value;
+			selectCount+=1;
+		}
+	}
+	if(selectCount==0){
+		return null;
+	}else{
+		return values;
+	}
+	
+}
